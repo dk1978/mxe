01df26f84452f5b4b837631b4c73410abe978bbd
Daniel Köster <dk@eada.de>
2019-11-24 21:40:24 -0800

    Add support for also building Google Mock library.

--- src/googletest.mk
+++ src/googletest.mk
@@ -8,12 +8,10 @@ $(PKG)_VERSION  := 1.8.1
 $(PKG)_CHECKSUM := 9bf1fe5182a604b4135edc1a425ae356c9ad15e9b23f9f12a02e80184c3a249c
 $(PKG)_GH_CONF  := google/googletest/tags, release-
 $(PKG)_DEPS     := cc
-#$(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
-#$(PKG)_TYPE     := source-only
 
 define $(PKG)_BUILD
     cd '$(BUILD_DIR)' && '$(TARGET)-cmake' \
-          -DBUILD_GMOCK=off \
+          -DBUILD_GMOCK=on \
          '$(SOURCE_DIR)'
     $(MAKE) -C '$(BUILD_DIR)' -j '$(JOBS)'
     $(MAKE) -C '$(BUILD_DIR)' -j 1 install
